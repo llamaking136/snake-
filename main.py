1b491e197cb0ceffb4ebaef50976927cd21cecd6
llamaking136 <67032575+llamaking136@users.noreply.github.com>
2022-01-23 13:54:50 -0700

    IT FINALLY WORKS

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ try:
     from loguru import logger
     import time
     import enum
+    import random
 except ImportError:
     try:
         import pip
@@ -79,19 +80,41 @@ class Snake:
         self.x = x
         self.y = y
         self.grid = grid
-        self.length = 1
+        self.length = 3
         self.isDead = False
         self.direction = Direction.RIGHT
         self.boxes = [(x, y), (x - 1, y), (x - 2, y)]
 
+        self.head_pos = list(reversed(self.boxes))
+
+        self.food_pos = self.genRandomPos()
+
         self.grid.arr[x][y] = True
     
+    def isOnSnake(self, x, y):
+        for i in self.boxes:
+            if (x, y) == i:
+                return True
+        return False
+
+    def genRandomPos(self):
+        isOnSnake = False
+        pos = None
+        while not isOnSnake:
+            pos = (random.randint(0, self.grid.x - 1), random.randint(0, self.grid.y - 1))
+            if self.isOnSnake(pos[0], pos[1]):
+                continue
+            else:
+                break
+        return pos
+
     def eat(self): # TODO: FINISH
         self.length += 1
-        # get last boxes' position and set the new box back one position
-        lastbox = self.boxes[-1]
-        if lastbox[0] == 0 or lastbox[0] == self.grid.width - 1: # if at the left edge or at the right edge
-            newpos = [0, lastbox[1] + 1]
+        lastbox = self.head_pos[-1 - self.length]
+        # if lastbox[0] == 0 or lastbox[0] == self.grid.width - 1: # if at the left edge or at the right edge
+        self.boxes.append(lastbox)
+
+        self.food_pos = self.genRandomPos()
 
     def getDirection(self, direction, coord):
         if direction == Direction.UP:
@@ -107,7 +130,14 @@ class Snake:
         index = 0
         for i in self.boxes:
             old_i = i
-            coord = self.getDirection(self.direction, i)
+            if index == 0:
+                if i == self.food_pos:
+                    self.eat()
+                    logger.info("Ate food")
+                coord = self.getDirection(self.direction, i)
+                self.head_pos.append(coord)
+            else:
+                coord = self.head_pos[-1 - index]
             # turn the last box from on to off
             self.grid.arr[coord[0]][coord[1]] = True
             self.grid.arr[old_i[0]][old_i[1]] = None
@@ -115,23 +145,30 @@ class Snake:
 
             index += 1
 
-BOX_NUM_X  = 60
-BOX_NUM_Y  = 60
+BOX_NUM_X  = 30
+BOX_NUM_Y  = 30
 BOX_WIDTH  = WIDTH  / BOX_NUM_X
 BOX_HEIGHT = HEIGHT / BOX_NUM_Y
 
 grid = Grid(BOX_NUM_X, BOX_NUM_Y)
-snake = Snake(1, BOX_NUM_Y // 2, grid)
+snake1 = Snake(1, BOX_NUM_Y // 2, grid)
+snake2 = Snake(BOX_NUM_X - 2, BOX_NUM_Y // 2, grid)
+snake2.direction = Direction.LEFT
 
 snake1_color = (0, 255, 0)
 snake2_color = (0, 0, 255)
+food_color   = (255, 0, 0)
+
+def game_over():
+    # test function
+    exit(1)
 
 @logger.catch
 def main():
     x = 0
     y = HEIGHT // 2
 
-    sleep_time = 500
+    sleep_time = 125
     time_until_sleep = 0 # millisecond when we stop sleeping
     do_update = True
 
@@ -161,25 +198,47 @@ def main():
         SURFACE.fill((0, 0, 0))
 
         if do_update:
-            snake.update()
+            snake1.update()
+            snake2.update()
             do_update = False
             time_until_sleep = t + sleep_time
-        grid = snake.grid
-        for i in snake.boxes:
+        grid = snake1.grid
+        snake2.food_pos = snake1.food_pos
+        
+        # render food before snake because why not
+        pygame.draw.rect(SURFACE, food_color, (snake1.food_pos[0] * BOX_WIDTH, snake1.food_pos[1] * BOX_HEIGHT, BOX_WIDTH, BOX_HEIGHT))
+    
+        for i in snake1.boxes:
             pygame.draw.rect(SURFACE, snake1_color, (i[0] * BOX_WIDTH, i[1] * BOX_HEIGHT, BOX_WIDTH, BOX_HEIGHT))
+
+        for i in snake2.boxes:
+            pygame.draw.rect(SURFACE, snake2_color, (i[0] * BOX_WIDTH, i[1] * BOX_HEIGHT, BOX_WIDTH, BOX_HEIGHT))
 
         if not do_update:
             if t >= time_until_sleep:
                 do_update = True
 
+        if snake1.isDead or snake2.isDead:
+            logger.info("Snake died")
+            game_over()
+
         if getkeydown(K_UP):
-            snake.direction = Direction.UP
+            snake1.direction = Direction.UP
         elif getkeydown(K_DOWN):
-            snake.direction = Direction.DOWN
+            snake1.direction = Direction.DOWN
         elif getkeydown(K_LEFT):
-            snake.direction = Direction.LEFT
+            snake1.direction = Direction.LEFT
         elif getkeydown(K_RIGHT):
-            snake.direction = Direction.RIGHT
+            snake1.direction = Direction.RIGHT
+
+        if getkeydown(K_UP):
+            snake2.direction = Direction.DOWN
+        elif getkeydown(K_DOWN):
+            snake2.direction = Direction.UP
+        elif getkeydown(K_LEFT):
+            snake2.direction = Direction.RIGHT
+        elif getkeydown(K_RIGHT):
+            snake2.direction = Direction.LEFT
 
         pygame.display.flip()
         
